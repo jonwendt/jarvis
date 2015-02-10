004b439e1727d9b7c881be7ccf7603287175dd1e
Jon Wendt <jpwendt@asu.edu>
2015-02-10 01:55:20 -0700

    Speech more modularized.

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,14 +1,16 @@
 class Alarm < ActiveRecord::Base
+  include Speech
+
   def play_message
     message = self.message + ". The time is now #{Time.now.in_time_zone.strftime('%l:%M %P')}"
-    `#{Rails.root.join('bin')}/speech.sh "#{message}"`
+    say(message)
     return message
   end
 
   def self.schedule_alarms
     alarms = Alarm.where("days = 'ALL'") # Implement days as string
     alarms.each do |alarm|
-      time = Time.now.in_time_zone.to_date + alarm.time.hour.hours + alarm.time.min.minutes
+      time = Time.now.utc.to_date + alarm.time.hour.hours + alarm.time.min.minutes
       AlarmBellJob.perform_at(time, alarm.id)
     end
   end
