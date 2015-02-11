f96709764e718f86180b7f1b462ca3f08459e1ed
Jon Wendt <jpwendt@asu.edu>
2015-02-10 21:09:18 -0700

    Rewrote shell script in ruby.

--- app/models/concerns/speech.rb
+++ app/models/concerns/speech.rb
@@ -1,5 +1,15 @@
 module Speech
+  # Splits the message into chunks of 100 characters so Google's voice translate
+  # api will accept it and return an mp3, which is played with mpg123
   def say(message)
-    `#{Rails.root.join('bin')}/speech.sh "#{message}"`
+    chunk = '' # <= 100 characters
+    for word in message.split(' ')
+      if (chunk + ' ' + word).length > 100
+        `mpg123 -q "http://translate.google.com/translate_tts?tl=en&q=#{chunk}"`
+        chunk.clear
+      end
+      chunk += ' ' + word
+    end
+    `mpg123 -q "http://translate.google.com/translate_tts?tl=en&q=#{chunk}"`
   end
 end
